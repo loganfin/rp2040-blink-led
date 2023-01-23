3ad1be7a7f02c2a202cac0eab116acbf5ce5f8b9
Logan Finley <finley4.logan@gmail.com>
2023-01-23 11:07:59 -0800

    Got the LED blinking

--- src/main.c
+++ src/main.c
@@ -1,15 +1,33 @@
+#include "boards/adafruit_feather_rp2040.h"
 #include "pico/stdlib.h"
+
+#define LED_PIN PICO_DEFAULT_LED_PIN
 
 int main()
 {
-    const uint LED_PIN = 25;
+    const int dot_ms = 200;
+    const int dash_ms = 400;
+    int wait_ms = 0;
+    int counter = 0;
+
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
     while (true) {
+        if (counter < 3) {
+            wait_ms = dot_ms;
+        }
+        else if (counter < 6) {
+            wait_ms = dash_ms;
+        }
+        else {
+            counter = 0;
+            continue;
+        }
         gpio_put(LED_PIN, 1);
-        sleep_ms(250);
+        sleep_ms(wait_ms);
         gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        sleep_ms(wait_ms);
+        counter++;
     }
 }
